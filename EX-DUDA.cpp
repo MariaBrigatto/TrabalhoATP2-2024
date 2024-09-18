a490361072cb662c3472459f22dba766cdf0c1f0
MariaBrigatto <maria.carnelosbrigatto@gmail.com>
2024-09-17 21:36:35 -0300

    Arquivo Raul (Duda)

---
+++ EX-DUDA.cpp
@@ -0,0 +1,28 @@
+#include "bibliotecas.h"
+#define TF 50
+
+
+// ---* TRABALHO DE ATP2 DO RAUL MAMACO E DUDA ORANGOS *---
+
+
+
+struct TpAlunos {
+	
+	char RA[10] , Nome[30]; // ---* CHAVE PRIMARIA RA
+	
+};
+
+struct TpDisciplinas {
+	
+	char CodDisc[10], Disciplina[30]; // ---* CHAVE PRIMARIA CodDisc
+	
+};
+
+struct TpAlunosDisciplinas { // ---* tabela de muitos para muitos
+	
+	char RA[10], CodDisc[10]; // ---* CHAVE PRIMARIA RA e CodDisc;
+	float Nota;
+	
+};
+
+
